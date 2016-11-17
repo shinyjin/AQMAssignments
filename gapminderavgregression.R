cf141f97b3f7aab976726a2f412bc95e2c042419
shinyjin <shiny_jin@alumni.ubc.ca>
2016-11-16 17:43:44 -0800

    first commit

--- gapminderavgregression.R
+++ gapminderavgregression.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(ggplot2)
 library("gapminder")
 gapminder
 head(gapminder)
@@ -8,7 +9,17 @@ countries <- as.character(unique(unlist(gapminder$country)))
 new <- gapminder %>%
   filter(year >= 1952) %>%
   group_by(country)%>%
-  summarize(meangdpPercap = mean(gdpPercap),meanlifeExp=mean(lifeExp))
+  summarize(meangdpPercap = mean(gdpPercap),meanlifeExp=mean(lifeExp),country=country)
 
-reg <- lm(meanlifeExp~meangdpPercap, data = new)  
+reg <- lm(meanlifeExp~meangdpPercap, data = new) 
+r <- residuals(reg)
+plot(r)
+g <- ggplot(data = new, aes(x=meangdpPercap, y=meanlifeExp))
+l <- ggplot(data=new, aes(x=log(meangdpPercap),y = meanlifeExp))
+logreg <- lm(meanlifeExp~log(meangdpPercap), data = new) 
+R <- residuals(logreg)
+summary(logreg)
+g+geom_point(alpha = 1/2)+geom_smooth(method="lm",color="pink")
+l+geom_point(alpha = 1/2)+geom_smooth(method="glm",color="blue")
+
 summary(reg)
